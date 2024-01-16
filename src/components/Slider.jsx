05904bd392ee4d052efe842165fa26a027abbcdd
alikubanov <alikubanov15@gmail.com>
2024-01-16 09:58:40 +0600

    fix:error Slider.css on Sllider.css

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Slider.css";
+import "./Sllider.css";
 import "react-slideshow-image/dist/styles.css";
 import { Fade, Zoom, Slide } from "react-slideshow-image";
 import leftArrow from "../assets/images/left-arrow.svg";
